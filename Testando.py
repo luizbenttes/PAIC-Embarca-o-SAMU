8bd866ec217a8c016881c6c811d10afc7ef00ec2
Luiz Bentes <luizfbenttes@gmail.com>
2017-08-24 19:47:20 -0400

    Adição do ângulo de Costado

--- Testando.py
+++ Testando.py
@@ -4,18 +4,30 @@ import math
 
 def AngToRad(ang):
     return ang * 2 * math.pi / 360.0
-    
-deadrise = int(input("Insira o angulo de Deadrise: "))
-x = np.arange(10)
-t = math.tan(AngToRad(deadrise))
 
-plt.plot(x, t * x)
-plt.legend(['y ='+str(t)+'x'], loc='upper left')
+
+def AngComplement(ang):
+    return 90 - ang
+
+deadrise = int(input("Insira o angulo de Deadrise: "))
+x = np.arange(0,20,1)
+d = np.tan(AngToRad(deadrise))
+
+plt.plot(x, d * x, 'g')
+plt.legend(['y ='+str(d)+'x'], loc='upper left')
 plt.grid(True)
 #plt.show()
 
-largura = input("Insira a largura da meia boca")
-#costado = int(input("Insira o angulo de Costado: "))
+#largura = int(input("Insira a largura da meia boca: "))
+costado = int(input("Insira o angulo de Costado: "))
+costado = AngComplement(costado)
+c = np.tan(AngToRad(costado))
+
+plt.plot(x, c * x, 'r')
+
+#plt.axvline(x = largura)
+
+plt.show()
 #plt.plot(x , 5 * x + 3)
 #plt.show()
 
